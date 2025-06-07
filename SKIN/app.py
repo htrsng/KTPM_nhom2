82e8d07ac385298258732b9879b340990e73e4a9
Kaitszo <thanhtb2005@gmail.com>
2025-06-07 22:30:07 +0700

    cập nhật thêm phần hỗ trợ cho da khô

--- SKIN/app.py
+++ SKIN/app.py
@@ -16,7 +16,8 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %
 logger = logging.getLogger(__name__)
 
 # Path to the model
-MODEL_PATH = r"D:\KTPM_nhom2\SKIN\models\best.pt"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+MODEL_PATH = os.path.join(BASE_DIR, "models", "best.pt")
 
 # Load YOLOv8 model
 try:
